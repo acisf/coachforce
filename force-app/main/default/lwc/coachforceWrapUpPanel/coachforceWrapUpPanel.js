d7b701c6f22846c803fa5f163b6b31f72dc43cd9
Doug <dbvann@gmail.com>
2026-03-29 20:13:20 -0400

    wrapupPanel status

--- force-app/main/default/lwc/coachforceWrapUpPanel/coachforceWrapUpPanel.js
+++ force-app/main/default/lwc/coachforceWrapUpPanel/coachforceWrapUpPanel.js
@@ -5,22 +5,38 @@ export default class CoachforceWrapUpPanel extends LightningElement {
     @api recordId;
 
     isSaving = false;
+    statusMessage = 'Saving your call';
+    dotCount = 0;
+    statusInterval;
+
+    statusSteps = [
+        'Saving your call',
+        'Analyzing transcript',
+        'Scoring against evaluation criteria',
+        'Generating coaching session'
+    ];
 
     get saveButtonLabel() {
         return this.isSaving ? 'Saving...' : 'Save';
     }
 
+    get animatedStatusMessage() {
+        return `${this.statusMessage}${'.'.repeat(this.dotCount + 1)}`;
+    }
+
     handleSubmit() {
         this.isSaving = true;
+        this.startSavingStatus();
     }
 
     handleSuccess() {
         this.isSaving = false;
+        this.stopSavingStatus();
 
         this.dispatchEvent(
             new ShowToastEvent({
                 title: 'Saved',
-                message: 'Call wrapped up!',
+                message: 'Call wrapped up! Check for a coaching session in your notifications.',
                 variant: 'success'
             })
         );
@@ -28,6 +44,7 @@ export default class CoachforceWrapUpPanel extends LightningElement {
 
     handleError(event) {
         this.isSaving = false;
+        this.stopSavingStatus();
 
         let message = 'Unable to save call wrap-up.';
         if (event?.detail?.message) {
@@ -41,5 +58,37 @@ export default class CoachforceWrapUpPanel extends LightningElement {
                 variant: 'error'
             })
         );
+    }
+
+    startSavingStatus() {
+        this.stopSavingStatus();
+
+        let stepIndex = 0;
+        this.statusMessage = this.statusSteps[stepIndex];
+        this.dotCount = 0;
+
+        this.statusInterval = window.setInterval(() => {
+            this.dotCount = (this.dotCount + 1) % 3;
+
+            if (stepIndex < this.statusSteps.length - 1) {
+                stepIndex += 1;
+                this.statusMessage = this.statusSteps[stepIndex];
+            } else {
+                this.statusMessage = this.statusSteps[this.statusSteps.length - 1];
+            }
+        }, 4000);
+    }
+
+    stopSavingStatus() {
+        if (this.statusInterval) {
+            clearInterval(this.statusInterval);
+            this.statusInterval = null;
+        }
+        this.statusMessage = 'Saving your call';
+        this.dotCount = 0;
+    }
+
+    disconnectedCallback() {
+        this.stopSavingStatus();
     }
 }
